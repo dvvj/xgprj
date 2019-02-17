0812f9227036e3c576a373a9bf9d7a241cad2630
devvj <devvjj5@gmail.com>
2019-02-18 00:39:55 +0100

    fonts

---
+++ ui/src/main/java/org/xg/ui/UiLogin.java
@@ -0,0 +1,58 @@
+package org.xg.ui;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class UiLogin extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Label lblUid = new Label("用户 ID");
+    Label lblUType = new Label("用户类型");
+    Label lblPass = new Label("登录密码");
+    VBox vboxLabels = new VBox(lblUid, lblUType, lblPass);
+    vboxLabels.setAlignment(Pos.BASELINE_RIGHT);
+    vboxLabels.setLayoutX(20);
+    vboxLabels.setLayoutY(20);
+    vboxLabels.setSpacing(10);
+
+    Text txtUid = new Text();
+    Text txtUType = new Text("todo");
+    PasswordField pass = new PasswordField();
+    pass.setPromptText("您的密码");
+
+    VBox vboxInputs = new VBox(txtUid, txtUType, pass);
+    vboxInputs.setSpacing(10);
+
+    HBox hboxAll = new HBox(vboxLabels, vboxInputs);
+    hboxAll.setSpacing(20);
+    hboxAll.setPadding(new Insets(40));
+    hboxAll.setAlignment(Pos.CENTER);
+
+
+    Scene scene = new Scene(hboxAll);
+    scene.setFill(Color.TRANSPARENT);
+    scene.getStylesheets().add(
+      getClass().getResource("/default.css").toExternalForm()
+    );
+
+    stage.setScene(scene);
+
+    stage.show();
+  }
+}
