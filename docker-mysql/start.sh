1b50aa0a3791b0bc45c2a9cdcab6a8be1f6066cd
devvj <devvjj5@gmail.com>
2019-02-17 08:21:51 +0100

    test add dbuser

--- docker-mysql/start.sh
+++ docker-mysql/start.sh
@@ -1,1 +1,5 @@
-docker run -d -p 3306:3306 --name xgsql_i1 -e MYSQL_ROOT_PASSWORD=cPEaKeXnzq8fuRBD87csHdaL xgsql
+docker run -d -p 3306:3306 --name xgsql_i1 \
+  -e MYSQL_ROOT_PASSWORD=cPEaKeXnzq8fuRBD87csHdaL \
+  -e MYSQL_USER=dbuser \
+  -e MYSQL_PASSWORD=dbpass \
+  xgsql
