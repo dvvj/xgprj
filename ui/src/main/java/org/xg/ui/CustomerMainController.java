03ffc48dd5e5c272b04374f29f0c017006dcce2d
devvj <devvjj5@gmail.com>
2019-02-21 01:05:06 +0100

    resource bundles

---
+++ ui/src/main/java/org/xg/ui/CustomerMainController.java
@@ -0,0 +1,31 @@
+package org.xg.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerMainController {
+
+  @FXML
+  private Text txtTest;
+
+  @FXML
+  private URL location;
+
+  @FXML
+  private ResourceBundle resources;
+
+  @FXML
+  public void initialize() {
+    String txt = String.format(
+      "%s\n%s",
+      location.toString(),
+      resources.getBaseBundleName()
+    );
+    txtTest.setText(txt);
+    //resourcesLabel.setText(resources.getBaseBundleName());
+  }
+
+}
--- ui/src/main/java/org/xg/ui/UiLoginController.java
+++ ui/src/main/java/org/xg/ui/UiLoginController.java
@@ -2,15 +2,21 @@ package org.xg.ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.xg.auth.AuthResp;
 import org.xg.auth.SvcHelpers;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class UiLoginController {
   @FXML
@@ -30,6 +36,27 @@ public class UiLoginController {
     this.stage = stage;
   }
 
+  private void launchMain(String fxmlMain) {
+    try {
+      FXMLLoader loader = new FXMLLoader(
+        getClass().getResource(fxmlMain)
+      );
+      loader.setResources(
+        ResourceBundle.getBundle("ui.CustomerMain")
+      );
+      HBox root = loader.load();
+      CustomerMainController controller = loader.getController();
+
+      Scene scene = new Scene(root);
+      Stage mainStage = new Stage();
+      mainStage.setScene(scene);
+      mainStage.show();
+    }
+    catch (IOException ex) {
+      throw new RuntimeException("Error launching main window!", ex);
+    }
+  }
+
   public void onLogin(ActionEvent e) {
     //System.out.println("login button pressed");
     String authUrl = "https://localhost:8443/webapi/auth/userPass";
@@ -44,6 +71,8 @@ public class UiLoginController {
       System.out.println(resp.token());
       txtStatus.setText("success");
       txtStatus.setStroke(Color.GREEN);
+      launchMain("/ui/CustomerMain.fxml");
+      stage.close();
     }
     else {
       txtStatus.setText("failed");
