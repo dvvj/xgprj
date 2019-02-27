a7faf8baba3f21d92bc680e882ec9cebd94b2334
devvj <devvjj5@gmail.com>
2019-02-27 15:01:19 +0100

    db backup dir

--- docker-mysql/start.sh
+++ docker-mysql/start.sh
@@ -2,4 +2,5 @@ docker run -d -p 3306:3306 --name xgsql_i1 \
   -e MYSQL_ROOT_PASSWORD=cPEaKeXnzq8fuRBD87csHdaL \
   -e MYSQL_USER=dbuser \
   -e MYSQL_PASSWORD=dbpass \
+  -v $1:/app-backup \
   xgsql
--- test-utils/start-svrs.sh
+++ test-utils/start-svrs.sh
@@ -1,4 +1,4 @@
 cd ../docker-mysql/
-sh ./start.sh
+sh ./start.sh /home/devvj/db-backup
 cd ../svc/
 sh ./start-svc-ssl.sh /home/devvj/prod_assets
