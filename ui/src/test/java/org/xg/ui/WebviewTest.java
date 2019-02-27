3636823a23a24c5d54d6c2c320dc7a6eac163deb
devvj <devvjj5@gmail.com>
2019-02-27 02:30:45 +0100

    t

--- ui/src/test/java/org/xg/ui/WebviewTest.java
+++ ui/src/test/java/org/xg/ui/WebviewTest.java
@@ -2,10 +2,12 @@ package org.xg.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import org.w3c.dom.Document;
 import org.xg.pay.AlipayCfg;
 import org.xg.pay.AlipayHelpers;
 import org.xg.ui.utils.Global;
@@ -18,12 +20,22 @@ public class WebviewTest extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    Pane root = new HBox();
+    HBox root = new HBox();
+    root.setSpacing(20);
     WebView wv = new WebView();
     wv.getEngine().setUserStyleSheetLocation(
       getClass().getResource("/default.css").toExternalForm()
     );
-    root.getChildren().add(wv);
+
+    Button btn = new Button();
+    btn.setText("inspect");
+    btn.setOnAction(e -> {
+      //Document doc = wv.getEngine().getDocument();
+      String html = (String) wv.getEngine().executeScript("document.documentElement.outerHTML");
+      System.out.println(html);
+    });
+
+    root.getChildren().addAll(btn, wv);
 
     AlipayCfg cfg = AlipayHelpers.testLocalCfg();
     String pageContent = AlipayHelpers.test1(cfg);
