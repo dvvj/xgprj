7f36f678bbf321c980b12cd8a7b2da341533e29b
devvj <devvjj5@gmail.com>
2019-03-04 12:12:19 +0100

    order test (in progress)

--- hbn-tests/src/test/scala/org/xg/hbn/PlaceOrderTests.scala
+++ hbn-tests/src/test/scala/org/xg/hbn/PlaceOrderTests.scala
@@ -12,66 +12,101 @@ object PlaceOrderTests extends App {
   val uid2 = "customer2"
   val uid3 = "customer3"
 
-  val testData = Map(
-    uid1 -> Array(
-      Order4Test(uid1, 1, 2.0),
-      Order4Test(uid1, 2, 4.0),
-      Order4Test(uid1, 3, 6.0),
-      Order4Test(uid1, 4, 8.0)
+  val orderTestData = Seq(
+    Map(
+      uid1 -> Array(
+        Order4Test(uid1, 1, 2.0),
+        Order4Test(uid1, 2, 4.0),
+        Order4Test(uid1, 3, 6.0),
+        Order4Test(uid1, 4, 8.0)
+      ),
+      uid2 -> Array(
+        Order4Test(uid2, 1, 2.0),
+        Order4Test(uid2, 2, 4.0),
+        Order4Test(uid2, 3, 6.0),
+        Order4Test(uid2, 4, 8.0)
+      ),
+      uid3 -> Array(
+        Order4Test(uid3, 1, 3.0),
+        Order4Test(uid3, 2, 6.0),
+        Order4Test(uid3, 3, 9.0),
+        Order4Test(uid3, 4, 12.0)
+      )
     ),
-    uid2 -> Array(
-      Order4Test(uid2, 1, 2.0),
-      Order4Test(uid2, 2, 4.0),
-      Order4Test(uid2, 3, 6.0),
-      Order4Test(uid2, 4, 8.0)
+    Map(
+      uid1 -> Array(
+        Order4Test(uid1, 1, 12.0),
+        Order4Test(uid1, 2, 14.0),
+        Order4Test(uid1, 3, 16.0),
+        Order4Test(uid1, 4, 18.0)
+      ),
+      uid2 -> Array(
+        Order4Test(uid2, 1, 12.0),
+        Order4Test(uid2, 2, 14.0),
+        Order4Test(uid2, 3, 16.0),
+        Order4Test(uid2, 4, 18.0)
+      )
     ),
-    uid3 -> Array(
-      Order4Test(uid3, 1, 3.0),
-      Order4Test(uid3, 2, 6.0),
-      Order4Test(uid3, 3, 9.0),
-      Order4Test(uid3, 4, 12.0)
+    Map(
+      uid1 -> Array(
+        Order4Test(uid1, 1, 22.0),
+        Order4Test(uid1, 2, 24.0),
+        Order4Test(uid1, 3, 26.0),
+        Order4Test(uid1, 4, 28.0)
+      ),
+      uid3 -> Array(
+        Order4Test(uid3, 1, 22.0),
+        Order4Test(uid3, 2, 24.0),
+        Order4Test(uid3, 3, 26.0),
+        Order4Test(uid3, 4, 28.0)
+      )
     )
   )
 
-  import collection.mutable
-  val allUsers = Seq(uid1, uid2, uid3)
-  var expResultOrders = mutable.Map[String, Array[Order4Test]]()
+  def runTest(testData:Map[String, Array[Order4Test]]):Unit = {
+    import collection.mutable
 
-  def ordersOf(users:Iterable[String]):Map[String, Array[Order4Test]] = {
-    users.map { uid =>
-      val orders = hbnOps.ordersOf(uid)
-      uid -> orders.map(o => Order4Test(o.uid, o.productId, o.qty))
-    }.toMap
+    val allUsers = testData.keySet.toSeq
+    var expResultOrders = mutable.Map[String, Array[Order4Test]]()
+
+    def ordersOf(users:Iterable[String]):Map[String, Array[Order4Test]] = {
+      users.map { uid =>
+        val orders = hbnOps.ordersOf(uid)
+        uid -> orders.map(o => Order4Test(o.uid, o.productId, o.qty))
+      }.toMap
+    }
+
+    testSchedules(
+      () => {
+        expResultOrders = mutable.Map[String, Array[Order4Test]]()
+        val existingOrders = ordersOf(allUsers)
+        //expResultOrders ++= existingOrders
+        allUsers.foreach { uid =>
+          val existing = existingOrders.getOrElse(uid, Array())
+          val added = testData.getOrElse(uid, Array())
+          expResultOrders += uid -> (existing ++ added)
+        }
+      },
+      () => {
+        val s1 = placeOrderSchedule(testData(uid1))
+        runSchedulesAndWait(
+          allUsers.map(uid => placeOrderSchedule(testData(uid))),
+          60
+        )
+      },
+      () => {
+        val currOrders = ordersOf(allUsers)
+        if (checkOrders(currOrders, expResultOrders.toMap)) {
+          println(s"Test passed")
+        }
+        else {
+          println(s"Test Failed")
+          throw new RuntimeException("Test failed!")
+        }
+      }
+    )
   }
 
-  testSchedules(
-    () => {
-      expResultOrders = mutable.Map[String, Array[Order4Test]]()
-      val existingOrders = ordersOf(allUsers)
-      //expResultOrders ++= existingOrders
-      allUsers.foreach { uid =>
-        val existing = existingOrders.getOrElse(uid, Array())
-        val added = testData.getOrElse(uid, Array())
-        expResultOrders += uid -> (existing ++ added)
-      }
-    },
-    () => {
-      val s1 = placeOrderSchedule(testData(uid1))
-      runSchedulesAndWait(
-        allUsers.map(uid => placeOrderSchedule(testData(uid))),
-        60
-      )
-    },
-    () => {
-      val currOrders = ordersOf(allUsers)
-      if (checkOrders(currOrders, expResultOrders.toMap)) {
-        println(s"Test passed")
-      }
-      else {
-        println(s"Test Failed")
-      }
-    }
-  )
 
   def checkOrders(m1:Map[String, Array[Order4Test]], m2:Map[String, Array[Order4Test]]):Boolean = {
     val usrs1 = m1.keySet
@@ -93,6 +128,11 @@ object PlaceOrderTests extends App {
           true
       }
     }
+  }
+
+  orderTestData.foreach { orderTestData =>
+    runTest(orderTestData)
+    Thread.sleep(500)
   }
 
 //
