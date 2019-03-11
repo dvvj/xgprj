fdee114f4d246c65ca79518cb74bd97fe36c3854
devvj <devvjj5@gmail.com>
2019-03-11 11:28:14 +0100

    minor fix

--- utils/src/test/scala/org/xg/dbUtils/InsertCustomersUtil.scala
+++ utils/src/test/scala/org/xg/dbUtils/InsertCustomersUtil.scala
@@ -6,13 +6,13 @@ object InsertCustomersUtil extends App {
 
   //INSERT INTO customers (id, name, idcard_no, mobile, postal_addr, bday) VALUES ('uid=customer1', '张晓东', '3102030222313322', '13892929133', '邮寄地址1','1983-02-05');
 
-  val testRefUid = "TestRef-4ae8f129bf1c"
+//  val testRefUid = "TestRef-4ae8f129bf1c"
 
   val testCustomers = Array(
-    Array("customer1", "张晓东", "3102030222313322", "13892929133", "邮寄地址1", "1983-02-05", testRefUid) -> "123",
-    Array("customer2", "张晓", "31020555555555555", "1385555555", "邮寄地址2", "1983-02-05", testRefUid) -> "456",
-    Array("customer3", "晓东", "3102036666666666", "1386666666", "邮寄地址3", "1983-12-03", testRefUid) -> "abcdef",
-    Array("customer4", "王丽", "3102033333333333", "13833333333", "邮寄地址4", "1983-12-03", testRefUid) -> "acf"
+    Array("customer1", "张晓东", "3102030222313322", "13892929133", "邮寄地址1", "1983-02-05", "prof1") -> "123",
+    Array("customer2", "张晓", "31020555555555555", "1385555555", "邮寄地址2", "1983-02-05", "prof1") -> "456",
+    Array("customer3", "晓东", "3102036666666666", "1386666666", "邮寄地址3", "1983-12-03", "prof1") -> "abcdef",
+    Array("customer4", "王丽", "3102033333333333", "13833333333", "邮寄地址4", "1983-12-03", "prof2") -> "acf"
   )
 
   val insertStatementTemplate =
