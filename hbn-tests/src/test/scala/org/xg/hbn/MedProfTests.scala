a13eab245c340f3321399d6f5ca16eceff255b45
devvj <devvjj5@gmail.com>
2019-03-11 11:24:08 +0100

    med profs

--- hbn-tests/src/test/scala/org/xg/hbn/MedProfTests.scala
+++ hbn-tests/src/test/scala/org/xg/hbn/MedProfTests.scala
@@ -6,38 +6,54 @@ object MedProfTests extends App {
 
   import HbnDbOpsImpl._
 
-  val profId = "medprof3"
-  testHbnOps.addNewMedProf(
-    profId, "微信", "456", "232312l2222", "322233211"
-  )
+  private def testRefId():Unit = {
+    val profId = "profxxx"
+    testHbnOps.addNewMedProf(
+      profId, "微信", "456", "232312l2222", "322233211"
+    )
 
-  val m = testHbnOps.getMedProfPassMap
-  println(m.size)
+    val m = testHbnOps.getMedProfPassMap
+    println(m.size)
 
-  val i = 10
-  val params = Array(
-    s"_customer$i",
-    s"_name$i",
-    s"_pass$i",
-    s"_idCardNo$i",
-    s"_mobile$i",
-    s"_postalAddr$i",
-    profId,
-    "1991-10-02"
-  )
-  testHbnOps.addNewCustomer(
-    params(0),
-    params(1),
-    params(2),
-    params(3),
-    params(4),
-    params(5),
-    params(6),
-    params(7)
-  )
+    val i = 10
+    val params = Array(
+      s"_customer$i",
+      s"_name$i",
+      s"_pass$i",
+      s"_idCardNo$i",
+      s"_mobile$i",
+      s"_postalAddr$i",
+      profId,
+      "1991-10-02"
+    )
+    testHbnOps.addNewCustomer(
+      params(0),
+      params(1),
+      params(2),
+      params(3),
+      params(4),
+      params(5),
+      params(6),
+      params(7)
+    )
 
-  val customers = testHbnOps.customersOf(profId)
-  println(customers.length)
+    val customers = testHbnOps.customersOf(profId)
+    println(customers.length)
+  }
+
+  private def showAllMedProfs():Unit = {
+    val medProfs = testHbnOps.allMedProfs
+    println(medProfs.mkString("\n"))
+  }
+
+  private def testCustomersRefedBy(refUid:String):Unit = {
+    val medProfs = testHbnOps.customersRefedBy(refUid)
+    println(s"Customers refed by [$refUid]:\n${medProfs.mkString("\n")}")
+  }
+
+  showAllMedProfs()
+  testCustomersRefedBy("prof1")
+  testCustomersRefedBy("prof2")
 
   HbnUtils.shutdownTest()
 
---
+++ utils/src/test/scala/org/xg/dbUtils/InsertMedProfsUtil.scala
@@ -0,0 +1,27 @@
+package org.xg.dbUtils
+
+import org.xg.auth.AuthHelpers
+import org.xg.dbUtils.InsertCustomersUtil.testRefUid
+
+object InsertMedProfsUtil extends App {
+  val testProfs = Array(
+    Array("prof1", "李卫东", "3302030222313322", "13792929133") -> "123",
+    Array("prof2", "张鑫", "33020555555555555", "1375555555") -> "456",
+    Array("prof3", "陈伟达", "3302036666666666", "1376666666") -> "abcdef",
+  )
+
+  val insertStatementTemplate =
+    "INSERT INTO med_profs (prof_id, name, idcard_no, mobile, pass_hash)" +
+      "  VALUES (%s);"
+
+  val insertStatements = testProfs.map { p =>
+    val (line, pass) = p
+    val passHash = AuthHelpers.sha512(pass)
+    val hashStr = AuthHelpers.hash2Str(passHash)
+    val params = line.mkString("'", "','", "'") + s",X'$hashStr'"
+    insertStatementTemplate.format(params)
+  }
+
+  println(insertStatements.mkString("\n"))
+
+}
