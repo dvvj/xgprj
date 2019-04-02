fcf081eee7cd35a64c400451fceb37a86ee2e44c
devvj <devvjj5@gmail.com>
2019-04-02 12:00:27 +0200

    test data tool (in progress)

--- utils/src/test/scala/org/xg/dbUtils/InsertMedProfOrgUtil.scala
+++ utils/src/test/scala/org/xg/dbUtils/InsertMedProfOrgUtil.scala
@@ -1,7 +1,6 @@
 package org.xg.dbUtils
 
 import org.xg.auth.AuthHelpers
-import org.xg.dbUtils.InsertProfOrgAgentUtil.insertStatements
 
 object InsertMedProfOrgUtil {
 
@@ -9,16 +8,22 @@ object InsertMedProfOrgUtil {
   val orgId1 = MedProfOrg.genUid("org1")
   val orgId2 = MedProfOrg.genUid("org2")
 
+  val testProfOrgs = IndexedSeq(
+    IndexedSeq(orgId1, "医药公司1", "医药公司1 info", "13792929133", "2018-03-04T16:00:28.179") -> "123",
+    IndexedSeq(orgId2, "医药公司2", "医药公司2 info", "1375555555", "2019-08-04T16:00:28.179") -> "456",
+  )
+
   def main(args:Array[String]):Unit = {
-    val testProfOrgs = Array(
-      Array(orgId1, "医药公司1", "医药公司1 info", "13792929133", "2018-03-04T16:00:28.179") -> "123",
-      Array(orgId2, "医药公司2", "医药公司2 info", "1375555555", "2019-08-04T16:00:28.179") -> "456",
-    )
+    insertMedProfOrgs(testProfOrgs)
+
+  }
+
+  def insertMedProfOrgs(rawData:IndexedSeq[(IndexedSeq[String], String)]):Unit = {
     val insertStatementTemplate =
       "INSERT INTO med_prof_orgs (org_id, name, info, phone, join_date, pass_hash)" +
         "  VALUES (%s);"
 
-    val insertStatements = testProfOrgs.map { p =>
+    val insertStatements = rawData.map { p =>
       val (line, pass) = p
       val passHash = AuthHelpers.sha512(pass)
       val hashStr = AuthHelpers.hash2Str(passHash)
--- utils/src/test/scala/org/xg/dbUtils/TestDataSet.scala
+++ utils/src/test/scala/org/xg/dbUtils/TestDataSet.scala
@@ -1,5 +1,6 @@
 package org.xg.dbUtils
 
+import org.xg.dbUtils.InsertMedProfOrgUtil.{orgId1, orgId2}
 import org.xg.user.UserType._
 
 object TestDataSet {
@@ -78,21 +79,21 @@ object TestDataSet {
 
 
   val allCustomers = Seq(
-    (1, 1, 1) -> (6 -> "朱"),
-    (1, 1, 2) -> (2 -> "秦"),
-    (1, 1, 3) -> (2 -> "尤"),
-    (1, 1, 4) -> (0 -> "许"),
-    (1, 2, 1) -> (2 -> "何"),
-    (1, 2, 2) -> (1 -> "吕"),
-    (1, 3, 1) -> (2 -> "施"),
-    (1, 3, 2) -> (2 -> "孔"),
-    (1, 3, 3) -> (1 -> "曹"),
-    (2, 1, 1) -> (2 -> "严"),
-    (2, 1, 2) -> (2 -> "华"),
-    (2, 1, 3) -> (2 -> "金"),
-    (2, 2, 1) -> (2 -> "魏"),
-    (3, 1, 1) -> (0 -> "陶"),
-    (3, 1, 2) -> (0 -> "姜")
+    (1, 1, 1) -> 6,
+    (1, 1, 2) -> 2,
+    (1, 1, 3) -> 2,
+    (1, 1, 4) -> 0,
+    (1, 2, 1) -> 2,
+    (1, 2, 2) -> 1,
+    (1, 3, 1) -> 2,
+    (1, 3, 2) -> 2,
+    (1, 3, 3) -> 1,
+    (2, 1, 1) -> 2,
+    (2, 1, 2) -> 2,
+    (2, 1, 3) -> 2,
+    (2, 2, 1) -> 2,
+    (3, 1, 1) -> 0,
+    (3, 1, 2) -> 0
   )
 
   val MedProfBase =  IndexedSeq(
@@ -101,6 +102,74 @@ object TestDataSet {
     IndexedSeq("伟", "33020555555555556", "1375555556") -> "456",
     IndexedSeq("伟达", "3302036666666666", "1376666666") -> "abcdef"
   )
+
+  val OrgAgentBase = IndexedSeq(
+    IndexedSeq("建华", "业务员1 info", "13792929133", "2018-03-04T16:00:28.179") -> "123",
+    IndexedSeq("芳芳", "业务员2 info", "1375555555", "2019-08-04T16:00:28.179") -> "456",
+    IndexedSeq("爱国", "业务员3 info", "13755555566", "2019-08-04T16:00:28.179") -> "456",
+    IndexedSeq("娜", "业务员4 info", "13755555577", "2019-08-04T16:00:28.179") -> "456",
+  )
+
+  val agent2ProfSurnameMap = Map(
+    (1, 1) -> "朱",
+    (1, 2) -> "秦",
+    (1, 3) -> "尤",
+    (1, 4) -> "许",
+    (2, 1) -> "何",
+    (2, 2) -> "吕",
+    (3, 1) -> "施"
+  )
+
+  val org2AgentData = IndexedSeq(
+    1 -> (4, "孔"),
+    2 -> (2, "曹"),
+    3 -> (1, "严")
+  )
+
+  val ProfOrgBase = IndexedSeq(
+    IndexedSeq("医药公司1", "医药公司1 info", "13792929133", "2018-03-04T16:00:28.179") -> "123",
+    IndexedSeq("医药公司2", "医药公司2 info", "1375555555", "2019-08-04T16:00:28.179") -> "456",
+    IndexedSeq("医药公司3", "医药公司3 info", "1375555533333", "2019-08-04T16:00:28.179") -> "456",
+  )
+
+  private def genOrgAgents(
+    orgIdx:Int,
+    surname:String,
+    rawData:IndexedSeq[(IndexedSeq[String], String)]
+  ):IndexedSeq[(IndexedSeq[String], String)] = {
+
+    rawData.indices.map { idx =>
+      val (arr, pass) = rawData(idx)
+      val aidx = idx+1
+      val orgId = proforgId(orgIdx)
+      val aid = agentId(orgIdx, aidx)
+      val name = surname + arr(0)
+      val info = arr(1)
+      val sfx = s"$orgIdx$aidx"
+      val phone = arr(2) + sfx
+      val joinDate = arr(3)
+      IndexedSeq(aid, orgId, name, info, phone, joinDate) -> pass
+    }
+  }
+
+  private def insertOrgAgentData():Unit = {
+    org2AgentData.foreach { p =>
+      val (orgIdx, agentData) = p
+      val (agentCount, surname) = agentData
+      val orgId = proforgId(orgIdx)
+
+      val data = OrgAgentBase.take(agentCount)
+      val genData = genOrgAgents(orgIdx, surname, data)
+      InsertProfOrgAgentUtil.insertOrgAgents(genData)
+    }
+
+    val orgData = ProfOrgBase.indices.map { idx =>
+      val (arr, pass) = ProfOrgBase(idx)
+      val orgId = proforgId(idx+1)
+      (IndexedSeq(orgId) ++ arr) -> pass
+    }
+    InsertMedProfOrgUtil.insertMedProfOrgs(orgData)
+  }
 
   private def insertCustomerData(coord:(Int, Int, Int), count:Int):Unit = {
     val (o, a, p) = coord
@@ -112,10 +181,11 @@ object TestDataSet {
     InsertCustomersUtil.insertCustomers(genData, _ => profId(o, a, p))
   }
 
-  private def insertProfData(coord:(Int, Int, Int), surname:String):Unit = {
+  private def insertProfData(coord:(Int, Int, Int)):Unit = {
     val (o, a, p) = coord
     val data = MedProfBase(p-1)
     val pid = profId(o, a, p)
+    val surname = agent2ProfSurnameMap(o -> a)
     val name = surname + data._1(0)
 
     val sfx = s"$o$a$p"
@@ -130,10 +200,12 @@ object TestDataSet {
   def main(args:Array[String]):Unit = {
     allCustomers.foreach { p =>
       println()
-      val (count, surname) = p._2
-      insertProfData(p._1, surname)
+      val count = p._2
+      insertProfData(p._1)
       insertCustomerData(p._1, count)
     }
+
+    insertOrgAgentData()
 //    InsertCustomersUtil.insertCustomers(o1a1p1Customers, _ => profId(1, 1, 1))
 //    println()
 //    InsertCustomersUtil.insertCustomers(o1a1p2Customers, _ => profId(1, 1, 2))
