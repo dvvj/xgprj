8213a0f26e1f6a8381edcf9753bed2f5bb46a2a3
devvj <devvjj5@gmail.com>
2019-04-15 14:21:30 +0200

    fix prod category data

--- utils/src/test/scala/org/xg/dbUtils/InsertProdCategories.scala
+++ utils/src/test/scala/org/xg/dbUtils/InsertProdCategories.scala
@@ -7,19 +7,29 @@ import org.xg.dbModels.IJStr
 
 object InsertProdCategories {
 
+  val insertStatementTemplate =
+    "INSERT INTO prod_categories (id, name, detailed_info)" +
+      "  VALUES (%s);"
+
   def main(args:Array[String]):Unit = {
     val rs = getClass.getResourceAsStream("/prod_categories.txt")
     val lines = IOUtils.readLines(rs, StandardCharsets.UTF_8)
     import collection.JavaConverters._
-    lines.asScala.filter(!_.trim.isEmpty).map { l =>
+    val insertSttms = lines.asScala.filter(!_.trim.isEmpty).map { l =>
       val parts = l.split("\\|\\|\\|")
       val categoryId = parts(0).trim.toInt
       val name = parts(1).trim
       val json = parts(2).trim
-      val m = IJStr.mapFromJsons(json)
-      println(m.byJ("zhs"))
+      val vs = List(
+        categoryId.toString, name, json
+      ).mkString("'", "','", "'")
+      insertStatementTemplate.format(vs)
+//      val m = IJStr.mapFromJsons(json)
+//      println(m.byJ("zhs"))
     }
     rs.close()
+
+    println(insertSttms.mkString("\n"))
   }
 
 }
