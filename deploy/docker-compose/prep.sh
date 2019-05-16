5e1f391bcddc0cc34dcd51c6dc726acca95bcf94
devvj <devvjj5@gmail.com>
2019-05-16 08:09:00 +0200

    new cfg for docker-compose

--- deploy/docker-compose/prep.sh
+++ deploy/docker-compose/prep.sh
@@ -1,4 +1,6 @@
 rm -rf ./tomcat
 mkdir ./tomcat
 cp ../../svc/target/svc.war ./tomcat
-
+cp -r ../../svc/target/svc ./tomcat/svc
+cp ./svc_cfg.json ./tomcat/svc/WEB-INF/
+cp ./hibernate.cfg.xml ./tomcat/svc/WEB-INF/
